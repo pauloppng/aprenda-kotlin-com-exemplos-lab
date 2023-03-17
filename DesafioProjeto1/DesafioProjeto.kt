f952c8bc06834855c63c97214ec5f48b3aba9cc8
pauloppng <contatopaulopereira0@gmail.com>
2023-03-16 22:21:20 -0300

    Atualização

--- DesafioProjeto1/DesafioProjeto.kt
+++ DesafioProjeto1/DesafioProjeto.kt
@@ -1,23 +1,22 @@
-
 fun main(){
-enum class Nivel {BASICO, INTEMEDIARIO, AVANCADO}
 
+    enum class Nivel {BASICO, INTEMEDIARIO, AVANCADO}
 
-open class ConteudoEducacional (vararg nomeCE: String, var duracao: Int)
+    open class ConteudoEducacional (vararg nomeCE: String, var duracao: Int)
 
-class Estudantes (val nomeE: String, val idE: Int)
+    class Estudantes (val nomeE: String, val idE: Int)
 
-data class Formacao (val nomeF: String, val nivelF: Nivel, var ContEdu: List<ConteudoEducacional>)
+    data class Formacao (val nomeF: String, val nivelF: Nivel, var ContEdu: List<ConteudoEducacional>)
 
-Estudantes("Paulo", 1)
-Estudantes("Marta", 2)
-ConteudoEducacional("Intro", 2)
-ConteudoEducacional("Aula 2", 4)
-ConteudoEducacional("Aula 3", 3)
-Formacao("Curso de Kotlin", "BASICO")
+    Estudantes("Paulo", 1)
+    Estudantes("Marta", 2)
+    ConteudoEducacional("Intro", 2)
+    ConteudoEducacional("Aula 2", 4)
+    ConteudoEducacional("Aula 3", 3)
+    Formacao("Curso de Kotlin", "BASICO")
 
-    fun matricular(matriculados:Estudantes){
-        matriculados: List  = mutableListOF<Estudantes>()
-        println("Todos os matriculados no curso $nomeCE são: $matriculados")
-    }
+        fun matricular(matriculados:Estudantes){
+            matriculados: List  = mutableListOF<Estudantes>()
+            println("Todos os matriculados no curso $nomeCE são: $matriculados")
+        }
 }
---
+++ DesafioProjeto1/DesafioV2.kt
@@ -0,0 +1,9 @@
+import kotlin.io.println
+
+fun main() {
+    class Estudante(var nome: String)
+    val estudante = Estudante("Paulo")
+    println (estudante.nome)   
+    
+}
+
